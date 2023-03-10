1244bcb40413d9a6e506359f552dfbb3641def44
Yashdew <yashdewangan123456@gmail.com>
2023-03-11 00:27:32 +0530

    added error modal

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,14 +1,14 @@
 import React from "react";
-import classes from "./UserList.module.css";
+import classes from "./UsersList.module.css";
 
 import Card from "../UI/Card";
 
 const UserList = (props) => {
     return (
-        <Card>
+        <Card className = {classes.users}>
             <ul>
                 {props.users.map( (user) => (
-                    <li>
+                    <li key={user.id}>
                         {user.name} ({user.age} years old)
                     </li>
                 ))}
